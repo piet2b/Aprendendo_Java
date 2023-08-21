021776997d24accdf174260ffaf2f22c03f5494a
piet2b <piet2bezerra+dev@gmail.com>
2023-08-21 19:44:24 -0300

    Initial commit

---
+++ Sala/App.java
@@ -0,0 +1,10 @@
+public class App {
+ public static void main(String[] args) throws Exception {
+ String name = "Pietra";
+ int age = 20;
+ double grade = 9.5f;
+ System.out.printf("Name: %s\n", name);
+ System.out.printf("age: %d\n", age);
+ System.out.printf("Grade: %f", grade);
+ }
+}
---
+++ Sala/InputOutputExample.java
@@ -0,0 +1,10 @@
+public class InputOutputExample{
+    public static void main (String[] args){
+         float value = 99.9f;
+        System.out.print("Hello World form print.\n");
+        System.out.println ("Hello World form println.\n");
+        System.out.println ("The value of Java is: "+value);
+        System.out.printf ("The value of Java is: %f"+value);
+    }
+
+}
