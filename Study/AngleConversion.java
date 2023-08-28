12c9c472ce3fb46791117dac5a8fd1624d68d75f
piet2b <piet2bezerra+dev@gmail.com>
2023-08-28 19:56:33 -0300

    Angle Conversion

---
+++ Study/AngleConversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AngleConversion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an angle in degrees: ");
+        double angleDegrees = scanner.nextDouble();
+
+        double angleRadians = angleDegrees * Math.PI / 180;
+
+        System.out.println("Angle in radians: " + angleRadians);
+
+        scanner.close();
+    }
+}
