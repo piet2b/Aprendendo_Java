cd7fc1ec64ae0c673745a851f1b3df9a5ac052ab
piet2b <piet2bezerra+dev@gmail.com>
2023-09-04 19:07:30 -0300

    exercicio

---
+++ ContadorPar.java
@@ -0,0 +1,21 @@
+package AprendendoJava;
+
+import java.util.Scanner;
+
+public class ContadorPar{
+        public static void main(String[] args){
+            Scanner scan = new Scanner(System.in);
+
+            int vetor[] = new int[10];
+            int contPar = 0;
+            
+            for( int i=0; i< vetor.length; i++){
+                System.out.printf("Digite o vetor[%d]: ",i);
+                vetor[i] = scan.nextInt();
+                if (vetor[i]%2==0){
+                    contPar++;
+                }
+            }
+            System.out.println("Quantidade de pares: "+contPar);
+        }
+    }
